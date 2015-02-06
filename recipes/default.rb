e9bfcd089dd539fb7a6d29e247c1674d0285b04b
Sam Xiao <sam.xs.xiao@gmail.com>
2015-02-06 14:58:17 -0800

    Few folders/files need to be removed during an upgrade run
    Signed-off-by: Sam Xiao <sam.xs.xiao@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,28 @@ user node['artifactory']['user'] do
 end
  
 package 'unzip'
- 
+
+# NOTE: Must run these before the unpacking step
+# Needs to delete these contents as they are needed to be replaced during an
+# upgrade.
+# We follow the guide from JFrog for upgrade process:
+# http://www.jfrog.com/confluence/display/RTF/Upgrading+Artifactory
+files_to_replace = ['webapps/artifactory.war']
+folders_to_replace = ['tomcat', 'bin']
+
+files_to_replace.each do |item|
+  file "#{node['artifactory']['dir']}/#{item}" do
+    action :delete
+  end
+end
+
+folders_to_replace.each do |item|
+  directory "#{node['artifactory']['dir']}/#{item}" do
+    recursive true
+    action :delete
+  end
+end
+
 ark 'artifactory' do
   action :put
   url node['artifactory']['url']
