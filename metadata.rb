d234195f4a27fac5ffa1da163d9679f2682d6050
Anurag Mohanty <amohanty@amplify.com>
2015-04-21 10:54:22 -0400

    specifying supported OS

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      "Sets up open-source artifactory"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.1.0"
 
+supports 'debian'
+
 depends "runit"
 depends "ark"
 depends "apt"
